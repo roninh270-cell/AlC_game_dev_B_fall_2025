604d806334c186587e3038df5394955706221388
roninh270-cell <ronin.h270@stu.nebo.edu>
2025-09-10 09:18:50 -0600

    balloon game and code
    (please fix the code)

---
+++ prototype 2 balloon POP/Assets/scripts/Balloon.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public int clickToPop = 3; // How many clicks till the ballon pops
+
+    public float scaleToIncrease = 0.15f; // scale incresed each time the ballon is clicked
+
+    public int scoreToGive; // score given for the popped ballon
+
+    private ScoreManager scoreManager; // A variable to reference the scoreManger
+
+    public GameObject popEffect; //Refrance pop effect particle system
+
+    void OnMouseDown()
+
+    //reduce clicks by one
+    clickToPop -=1;
+    /// <summary>
+    /// increase balloon size
+    transform.localScale+= Vector3.one * scaleToIncrease
+    /// check to see if the balloon has popped
+    If(clickToPop == 0)
+// scoreManager.IncreaseScoreText()
+Destroy(gameObject);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
