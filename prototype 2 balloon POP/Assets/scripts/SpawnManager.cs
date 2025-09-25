e7a457c8c6c318a46b2f18afbd6b15155b5360c7
roninh270-cell <ronin.h270@stu.nebo.edu>
2025-09-25 08:52:32 -0600

    balloon game

--- prototype 2 balloon POP/Assets/scripts/SpawnManager.cs
+++ prototype 2 balloon POP/Assets/scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] balloonPrefabs; //array to store balloon game objects
     public float startDelay = 0.5f;
     public float spawnInterval = 1.5f;
-    public float xRange = 10.0f;
+    public float xRange = 15.0f;
     public float yPos = -5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,7 +15,7 @@ public class SpawnManager : MonoBehaviour
     void SpawnRandomBalloon()
     {
         //Get a random position on the x-axis
-        Vector3 spawnPos = new Vector3(Random.Range(-xRange, xRange), yPos, 0);
+        Vector3 spawnPos = new Vector3(Random.Range((-xRange +5), xRange), yPos, 170);
         //pick random balloon from the balloon array
         int balloonIndex = Random.Range(0, balloonPrefabs.Length);
         // spawn the random balloon at the spawn position 
